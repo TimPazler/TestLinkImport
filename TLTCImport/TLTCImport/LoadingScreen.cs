23eab4489d5903d4e01c2db72c6b7ccb7798e7e0
Тимур Малов О <t.malov@bars-open.ru>
2022-03-11 21:41:00 +0300

    SOKPO-7746 Добавлена форма для отображении операции загрузки и добавлена новая git

---
+++ TLTCImport/TLTCImport/LoadingScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TLTCImport
+{
+    public partial class LoadingScreen : Form
+    {
+        //Форма для отображения загрузки, во переноса рез-ов в TestLink
+        public LoadingScreen()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Size = new Size(350, 100);
+            var lbl = new Label() { Text = "Выполняется перенос результатов в TestLink..." };
+            lbl.Size = new Size(350, 100);
+            lbl.TextAlign = ContentAlignment.MiddleCenter;
+            this.Controls.Add(lbl);           
+        }       
+    }
+}
