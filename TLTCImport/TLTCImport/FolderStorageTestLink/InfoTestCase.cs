a1dab73ee0fb0eddf7308e991a41fec0535c50d8
Тимур Малов О <t.malov@bars-open.ru>
2022-03-18 18:45:57 +0300

    SOKPO-7746 Классы перенесены в новую папку и добавлен класс для получения информации о тест кейсе

---
+++ TLTCImport/TLTCImport/FolderStorageTestLink/InfoTestCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TLTCImport.FolderStorageTestLink
+{
+    //Класс, содержащий id и имя тесткейса
+    public class InfoTestCase
+    {
+        public int idTestCase { get; set; }
+        public int externalIdTestCase { get; set; }
+        public string nameTestCase { get; set; }
+
+        public InfoTestCase(int idTestCase, int externalIdTestCase, string nameTestCase)
+        {
+            this.idTestCase = idTestCase;
+            this.externalIdTestCase = externalIdTestCase;
+            this.nameTestCase = nameTestCase;
+        }
+    }
+}
--- TLTCImport/TLTCImport/FolderStorageTestLink/Subfolder.cs
+++ TLTCImport/TLTCImport/FolderStorageTestLink/Subfolder.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TLTCImport.FolderStorageTestLink;
 
 namespace TLTCImport
 {
     //Класс, содержащий id и имя подпапки
+    //А также ссылку на другой массив с данными о тесткейсе
     public class Subfolder
     {
         public int idSubfolder { get; set; }
         public string nameSubfolder { get; set; }
+
+        public InfoTestCase[] arr;
 
         public Subfolder(int idSubfolder, string nameSubfolder)
         {
