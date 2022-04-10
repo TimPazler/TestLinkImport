ad5703e4553b6a44dd49c0898643f007f9771cb9
TimPazler <78204225+TimPazler@users.noreply.github.com>
2022-04-10 19:19:02 +0300

    Работа с деревом
    Добавлены методы для отображения только тех папок и тест кейсов, которые присутствуют в тест плане

--- TLTCImport/TLTCImport/Processes/TreeWithTestCases.cs
+++ TLTCImport/TLTCImport/Processes/TreeWithTestCases.cs
@@ -14,12 +14,18 @@ namespace TLTCImport
         private TestLink testLinkApi = TestLinkResult.testLinkApi;
 
         //Заполнение массива папками и тесткейсами
-        public Folder[] FillArrayWithData(int projectId)
-        {           
-            var folder = GetArrayAllFolders(projectId);         
-            var testCasesForfolders = GetTestCasesForfolders(folder);
-            return testCasesForfolders;
-        }      
+        public Folder[] FillArrayWithData(int projectId, int testPlanId, string projectName)
+        {
+            string prefixName = TestLinkResult.GetPrefixProjectByName(projectName);
+            var allTestCasesTestPlan = TestLinkResult.GetTestCasesToTestPlan(testPlanId, projectName);
+
+            var folder = GetArrayAllFolders(projectId);
+            var folders = GetTestCasesForfolders(folder);
+
+            ArrayTestsCasesByTestPlan(folders, allTestCasesTestPlan, prefixName);
+
+            return folders;
+        }        
 
         //Получение всех тест кейсов (заполнение массива только тест кейсами)
         private Folder[] GetTestCasesForfolders(Folder[] folders)
@@ -41,14 +47,116 @@ namespace TLTCImport
                 MessageBox.Show($"Непредвиденная ошибка! Попробуйте перезапустить программу! \r\nИли обратитесь к разработчику! \r\n Ошибка: {e.Message}");
             }
             return folders;
-        }      
+        }
+
+        //Рекурсия
+        //Перезаполнение массива, в котором содержаться только те кейсы, которые есть в тест плане
+        //Пустые папки удаляются из массива
+        private Folder[] ArrayTestsCasesByTestPlan(Folder[] folders, Dictionary<string, int> allTestCasesTestPlan, string prefixName)
+        {
+            foreach (var folder in folders)
+            {
+                Dictionary<string, int> testCasesField = new Dictionary<string, int>();
+
+                if (folder.testCases != null)
+                {
+                    //Получение тест кейсов по папке первого уровня 
+                    foreach (var testCase in folder.testCases)
+                    {
+                        if(testCase != null)
+                            testCasesField.Add(prefixName + "-" + testCase.externalIdTestCase, testCase.idTestCase);
+                    }
+
+                    //Если в тест плане нет тест кейса, такого же, который есть в проекте, то удаляем кейс из папки
+                    var newTestCasesField = DictionaryWithCasesForOneFoldersBasedOnTestPlan(folder, allTestCasesTestPlan, testCasesField);
+
+                    //Проверка что в папке есть кейсы
+                    FillArrayWithCasesBasedOnTestPlan(folder, newTestCasesField);
+                }
+
+                //Проходим подпапки
+                ArrayTestsCasesByTestPlan(folder.folders, allTestCasesTestPlan, prefixName);
+            }
+            return folders;
+        }
+
+        //Возвращаем новый словарь, содержащий кейсы, которые есть в папке и тест плане
+        private Dictionary<string, int> DictionaryWithCasesForOneFoldersBasedOnTestPlan(Folder folder, Dictionary<string, int> allTestCasesTestPlan, Dictionary<string, int> testCasesField)
+        {
+            Dictionary<string, int> newTestCasesField = new Dictionary<string, int>();
+
+            var numberCasesViewedInField = 0;
+            //Просмотр тест кейсов в тест плане
+            foreach (var testCaseTestPlan in allTestCasesTestPlan)
+            {
+                //Просмотр тест кейсов в папки
+                foreach (var testCaseField in testCasesField)
+                {
+                    //Если в тест плане есть такой же тест кейс, который есть в проекте                    
+                    if (testCaseTestPlan.Key == testCaseField.Key)
+                    {
+                        numberCasesViewedInField++;
+                        newTestCasesField.Add(testCaseField.Key, testCaseField.Value);
+                        break;
+                    }
+                }
+                //если в папка все тест кейсы относятся к тест плану, то
+                //прекращаем поиск
+                if (numberCasesViewedInField == testCasesField.Count)
+                    break;
+            }
+
+            //Возвращаем новый словарь, содержащий кейсы, которые есть в папке и тест плане
+            return newTestCasesField;
+        }
+
+        //Удаление всего массива тест кейсов или частично, если есть пустые элементы
+        private void FillArrayWithCasesBasedOnTestPlan(Folder folder, Dictionary<string, int> newTestCasesField)
+        {
+            List<InfoTestCase> newTestCases = new List<InfoTestCase>();
+
+            //если кейсов в словаре нет
+            if (newTestCasesField.Count == 0)
+                folder.testCases = null;
+            //если есть кейсы в словаре, то убираем из папки те тесты, 
+            //которых нет в словаре
+            else if (newTestCasesField.Count > 0)
+            {
+                int elementArr = 0;
+                foreach (var testCaseFolder in folder.testCases)
+                {
+                    if (testCaseFolder != null)
+                    {
+                        var numberCasesViewedInField = 0;
+                        foreach (var newTestCaseField in newTestCasesField)
+                        {
+                            //Если в тест плане есть такой же тест кейс, который есть в проекте                    
+                            if (testCaseFolder.idTestCase == newTestCaseField.Value)
+                            {
+                                newTestCases.Add(testCaseFolder);
+                                elementArr++;
+                                numberCasesViewedInField++;
+                                break;
+                            }
+                        }
+
+                        //Если кейса нет в словаре, то очищаем его из папки
+                        if (numberCasesViewedInField == 0)
+                            folder.testCases[elementArr] = null;
+                    }
+                }
+            }
+
+            //Перенос кейсов из массива в папку
+            folder.testCases = newTestCases.ToArray();
+        }        
 
         //Получить массив всех тесткейсов в папках
         private Folder[] GetArrayAllTestCasesInFolders(Folder[] folders)
         {
-           InfoTestCase[] testCase;
+            InfoTestCase[] testCase;
 
-           List<TestCaseFromTestSuite> testCaseAllInfo;
+            List<TestCaseFromTestSuite> testCaseAllInfo;
 
             //Сначала заполняем подпапки
             FillArrayAllTestCasesInSubfolders(folders);
@@ -68,7 +176,7 @@ namespace TLTCImport
                     //Проверка, что в подпапках такого кейса нет                   
                     if (TestCaseExistsInSubfolder(folder.folders, testCaseInfo.name))
                     {
-                        testCase[j] = new InfoTestCase(testCaseInfo.id, Int32.Parse(testCaseInfo.external_id), testCaseInfo.name);
+                        testCase[j] = new InfoTestCase(testCaseInfo.id, Int32.Parse(testCaseInfo.external_id), testCaseInfo.name);                        
                         j++;
                     }
                 }
